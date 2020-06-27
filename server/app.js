034525ef82f538eb8505879e61076f6c8d424556
Guntoro <iguntop@gmail.com>
2020-06-27 17:37:58 +0700

    move socket.io from server to app

--- server/app.js
+++ server/app.js
@@ -1,15 +1,56 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const router = require('./router')
-const errHandler = require('./middleware/errHandler')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = require("./router");
+const errHandler = require("./middleware/errHandler");
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-app.use(cors())
-app.use(express.urlencoded({extended : true}))
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/',router)
-app.use(errHandler)
+app.use("/", router);
+app.use(errHandler);
 
-module.exports = app;
+const users = {};
+
+io.on("connection", (socket) => {
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+    console.log("Client connected: " + socket.id);
+  }
+  socket.emit("yourID", socket.id);
+
+  io.sockets.emit("allUsers", users);
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+
+  //Drawing Canvas socket
+  socket.on("mouse", (data) => {
+    socket.broadcast.emit("mouse", data);
+  });
+
+  socket.on("clear", () => {
+    socket.broadcast.emit("clear");
+  });
+
+  socket.on("disconnect", () => console.log("Client Canvass has disconnected"));
+});
+
+module.exports = server;
--- server/bin/server.js
+++ server/bin/server.js
@@ -1,49 +1,7 @@
 const { request } = require("express");
 
-const app = require("../app.js");
+const server = require("../app.js");
 const port = process.env.PORT || 3000;
-
-const http = require("http");
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
-
-const users = {};
-
-io.on("connection", (socket) => {
-  if (!users[socket.id]) {
-    users[socket.id] = socket.id;
-    console.log("Client connected: " + socket.id);
-  }
-  socket.emit("yourID", socket.id);
-
-  io.sockets.emit("allUsers", users);
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-  });
-
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("hey", {
-      signal: data.signalData,
-      from: data.from,
-    });
-  });
-
-  socket.on("acceptCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-
-  //Drawing Canvas socket
-  socket.on("mouse", (data) => {
-    socket.broadcast.emit("mouse", data);
-  });
-
-  socket.on("clear", () => {
-    socket.broadcast.emit("clear");
-  });
-
-  socket.on("disconnect", () => console.log("Client Canvass has disconnected"));
-});
 
 server.listen(port, () => console.log("server is running on port" + port));
 
