e95dcda87f40571d5e85c3aab7d7c95c70834da0
Grady Wicoady <nervalgravie@gmail.com>
2020-06-27 11:56:42 +0800

    add login register form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import Axios from "axios";
 
-const LoginForm = () => {
+export default function Login() {
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+  
+  function login(){
+
+    const data = {
+      username,
+      password
+    }
+
+    Axios.post('http://localhost:3000/users/login',data)
+      .then(({data}) => {
+        console.log('login completed')
+      })
+      .catch(console.log)
+  }
+
   return (
-    <div>
-      
+    <div class="container ">
+      <div class="row justify-content-md-center">
+        <div class="col col-lg-3"></div>
+        <div class="col-6  py-md-5 bg-light shadow-lg p-3 mb-5 bg-white rounded">
+            <h1 class="text-center">Login</h1>
+            
+              <div class="input-group-prepend"> <span class="input-group-text">Username</span></div>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            
+            <div class="input-group-prepend"> <span class="input-group-text">Password</span></div>
+            <input
+              type="password"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button type="button" class="btn btn-primary btn-lg btn-block" onClick={login}>
+              Login
+            </button>
+        </div>
+        <div class="col col-lg-3"></div>
+      </div>
     </div>
   );
 }
-
-export default LoginForm;
--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Axios from 'axios'
 
-const RegisterForm = () => {
-  return (
-    <div>
-      
+export default function Register (){
+
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+  const [email,setEmail] = useState('')
+
+  function register(){
+    const data={
+      username,
+      email,
+      password
+    }
+
+    Axios.post('http://localhost:3000/users/register',data)
+      .then(({data}) => {
+        console.log('register completed')
+      })
+      .catch(console.log)
+  }
+
+    return(
+        <div class="container ">
+      <div class="row justify-content-md-center">
+        <div class="col col-lg-3"></div>
+        <div class="col-6  py-md-5 bg-light shadow-lg p-3 mb-5 bg-white rounded">
+            <h1 class="text-center">Register</h1>
+            
+              <div class="input-group-prepend"> <span class="input-group-text">Username</span></div>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            
+            <div class="input-group-prepend"> <span class="input-group-text">Email</span></div>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <div class="input-group-prepend"> <span class="input-group-text">Password</span></div>
+            <input
+              type="password"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button type="button" class="btn btn-primary btn-lg btn-block" onClick={register}>
+              Register
+            </button>
+        </div>
+        <div class="col col-lg-3"></div>
+      </div>
     </div>
-  );
+    )
 }
-
-export default RegisterForm;
