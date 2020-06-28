faaa0a42d844c1fa2656689fb710c427adef2359
goberman15 <akbarrmdhn94@gmail.com>
2020-06-29 05:36:48 +0700

    re-style detail work and commission

--- client/src/components/DetailCommission.js
+++ client/src/components/DetailCommission.js
@@ -3,6 +3,7 @@ import './DetailCommission.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { getCommissionDetail, newProject } from '../store/actions';
+import accounting from 'accounting-js'
 
 const DetailCommission = () => {
   const dispatch = useDispatch();
@@ -27,26 +28,28 @@ const DetailCommission = () => {
   }
 
   return (
-    <div className="container-fluid d-flex flex-column align-items-center pr-3 pl-3" style={{ marginTop: 75 }}>
+    <div className="container-fluid d-flex flex-column align-items-center pr-3 pl-3 mb-3" style={{ marginTop: 75 }}>
       <div className="w-100 d-flex justify-content-center row">
-        <div className="col-6">
+        <div className="col-6 text-center">
           <img
             src={commission.image_url}
             width="80%"
             alt={commission.title}
+            className="com-image"
           ></img>
         </div>
         <div className="col-6 com-detail d-flex flex-column align-items-center justify-content-around p-3">
           <p className="com-title">{commission.title}</p>
-          <p className="com-price">{commission.price}</p>
+          <p className="com-price">{accounting.formatMoney(commission.price, { symbol: 'Rp ', precision: 2, thousand: '.', decimal: ',' })}</p>
           <h3 className="text-left"><span className="badge badge-info p-2 com-category">{commission.category}</span></h3>
           <div className="apply-button">
             <p className="mb-0" onClick={createProject}>Apply for Commission</p>
           </div>
         </div>
       </div>
-      <div className="com-desc mt-3">
-        <p className="com-desc-text">{commission.description}</p>
+      <div className="com-desc mt-3 d-flex flex-column align-items-center p-3">
+        <h4>Description</h4>
+        <p className="com-desc-text mb-0">{commission.description}</p>
       </div>
     </div>
   );
--- client/src/components/DetailWork.js
+++ client/src/components/DetailWork.js
@@ -14,7 +14,7 @@ const DetailWork = () => {
   }, [dispatch, id]);
 
   return (
-    <div className="container-fluid d-flex justify-content-center align-items-center" style={{ marginTop: 75 }}>
+    <div className="container-fluid d-flex justify-content-center align-items-center mb-3" style={{ marginTop: 75 }}>
       <div className="w-100 row">
         <div className="col-8 d-flex justify-content-center align-items-center work-image-container">
           <img
@@ -24,11 +24,20 @@ const DetailWork = () => {
             className="work-image"
           ></img>
         </div>
-        <div className="col-4 d-flex flex-column justify-content-start w-100 work-desc p-5">
-          <p className="work-creator mb-5">Creator: <Link to={'/profile/'+work.username}><span>{work.username}</span></Link></p>
+        <div className="col-4 d-flex flex-column justify-content-start w-100 work-desc p-5 align-items-center">
+          <p className="work-creator mb-5">
+            Creator: <Link to={'/profile/'+work.username}>
+              <span className="creator-name">{work.username}</span>
+            </Link>
+          </p>
           <p className="work-title mb-5">{work.title}</p>
-          <p className="work-story mb-5">{work.description}</p>
-          <h4 className="work-category text-left">Category : <span class="badge badge-info p-2 work-category">{work.category}</span></h4>
+          <p className="work-story mb-5 text-center">{work.description}</p>
+          <h4 className="work-category mb-5">
+            Category : <span class="badge badge-info p-2 work-category">{work.category}</span>
+          </h4>
+          <p className="work-disclaimer text-center">
+            If you love this work, visit the creator page and apply for his/her commission (if any) to support them.
+          </p>
         </div>
       </div>
     </div>
