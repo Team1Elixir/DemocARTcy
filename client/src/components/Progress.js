4847053b3246c689b60ab113d37d0efe99502e59
goberman15 <akbarrmdhn94@gmail.com>
2020-06-29 04:42:44 +0700

    re-styling progress card

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -5,47 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getProgressClient } from '../store/actions';
 
 const Progress = () => {
-  const dummyProgress = [
-    {
-      id: 2,
-      title: 'Doodle Art',
-      status: 'onProgress',
-      price: '120000',
-      ClientId: 2,
-      ArtistId: 1,
-      client: {
-        id: 2,
-        username: 'heroman',
-        email: 'heroman@mail.com'
-      },
-      artist: {
-        id: 1,
-        username: 'hueyguey',
-        email: 'hueyguey@mail.com',
-        profile_url: 'https://avatarfiles.alphacoders.com/167/thumb-167148.jpg'
-      }
-    },
-    {
-      id: 2,
-      title: '3D Character Design',
-      status: 'Done',
-      price: '500000',
-      ClientId: 3,
-      ArtistId: 6,
-      client: {
-        id: 3,
-        username: 'dadaduy',
-        email: 'dadaduy@mail.com'
-      },
-      artist: {
-        id: 6,
-        username: 'blackpatuy',
-        email: 'patatuy@mail.com',
-        profile_url: 'https://avatarfiles.alphacoders.com/875/thumb-87589.png'
-      }
-    }
-  ]
-
   const dispatch = useDispatch();
   const projects = useSelector(state => state.progressClient)
 
@@ -54,7 +13,7 @@ const Progress = () => {
   }, [dispatch])
 
   return (
-    <div className="container d-flex flex-column align-items-center mb-3" style={{ marginTop: 75 }}>
+    <div className="container d-flex flex-column align-items-center mb-5" style={{ marginTop: 75 }}>
       <h1 className="progress-main-title">Progress Bar</h1>
       {
         projects.map(project => {
--- client/src/components/ProgressCard.js
+++ client/src/components/ProgressCard.js
@@ -21,36 +21,34 @@ const ProgressCard = ({ data }) => {
   };
 
   return (
-    <div className="d-flex w-100 flex-column align-items-center mt-3">
-      <div className="row w-100 progress-container d-flex align-items-center">
-        <div className="col-3 d-flex flex-column align-items-center">
-          <img
-            src='https://avatarfiles.alphacoders.com/875/thumb-87589.png'
-            alt="avatar"
-            className="artist-ava"
-            width="120"
-          ></img>
-          <p className="mb-0 text-pinky artist-name">{artist.username}</p>
-        </div>
-        <div className="col-3">
-          <p className="mb-0 text-pinky job-title text-center">{title}</p>
-        </div>
-        <div className="col-3">
-          <h2 className="mb-0 job-status p-3 text-center">
-            <span className="badge">{status}</span>
-          </h2>
-        </div>
-        <div className="col-3 d-flex justify-content-center">
-          {status === "onProgress" && (
-            <div
-              className="progress-button w-50 text-center"
-              onClick={toLiveSketch}
-            >
-              <p className="mb-0">Live Sketch</p>
-            </div>
-          )}
-          {status === "Done" && <Payment price={price} email={client.email} />}
-        </div>
+    <div className="row w-100 progress-container d-flex align-items-center mt-3">
+      <div className="col-3 d-flex flex-column align-items-center">
+        <img
+          src='https://avatarfiles.alphacoders.com/875/thumb-87589.png'
+          alt="avatar"
+          className="artist-ava"
+          width="120"
+        ></img>
+        <p className="mb-0 text-bluish artist-name">{artist.username}</p>
+      </div>
+      <div className="col-3">
+        <p className="mb-0 text-bluish job-title text-center">{title}</p>
+      </div>
+      <div className="col-3">
+        <h2 className="mb-0 job-status p-3 text-center">
+          <span className="badge">{status}</span>
+        </h2>
+      </div>
+      <div className="col-3 d-flex justify-content-center">
+        {status === "onProgress" && (
+          <div
+            className="progress-button w-50 text-center"
+            onClick={toLiveSketch}
+          >
+            <p className="mb-0">Live Sketch</p>
+          </div>
+        )}
+        {status === "Done" && <Payment price={price} email={client.email} />}
       </div>
     </div>
   );
