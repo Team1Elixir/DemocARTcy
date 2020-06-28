d7dbe1f16fbe1839800e505813b8dd393deaf2c7
goberman15 <akbarrmdhn94@gmail.com>
2020-06-28 10:43:44 +0700

    change in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import './App.css';
-import Progress from './components/Progress';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Login from './components/LoginForm'
+import Register from './components/RegisterForm'
 
 function App() {
   return (
-    <div className="App">
-      <Progress />
-    </div>
+    <Router>
+      <div className="header">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Switch>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
