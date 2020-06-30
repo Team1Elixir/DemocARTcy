7f7f96dfce8a2714085b68c17d54b9252a0a305d
Guntoro <iguntop@gmail.com>
2020-07-01 03:56:54 +0700

    end point to heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const server = axios.create({
-  //   baseURL: "https://shrouded-ridge-07983.herokuapp.com/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://shrouded-ridge-07983.herokuapp.com/",
+  //   baseURL: "http://localhost:3000/",
 });
 
 export default server;
