c08300df9d8c6d10c8056f43efe0e45c730292f7
achrams <ramadhanovic@gmail.com>
2020-07-01 14:19:47 +0700

    darkmode v2

--- client/src/components/MainCommission.js
+++ client/src/components/MainCommission.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAllCommissions } from '../store/actions'
 import { useSelector, useDispatch } from 'react-redux';
 import CommissionCard from './CommissionCard'
@@ -8,17 +8,24 @@ import './MainWorkCommission.css';
 const MainCommission = () => {
   const commissions = useSelector((state) => state.commissions)
   const loading = useSelector((state) => state.loading)
+  const [cardbg, setCardbg] = useState('#DBF5FA')
   const dispatch = useDispatch()
 
   useEffect(() => {
       dispatch(getAllCommissions())
+      if (localStorage.darkmode =='dark') {
+        setCardbg('black')
+      } else {
+        setCardbg('#DBF5FA')
+      }
   }, []);
 
-  if(loading) return (<div style={{ marginTop: 200, textAlign: 'center' }}> <Loader type='Grid' color='#023E8A' /> </div>)
+  if(loading) return (<div className='loadingscreen' style={{ textAlign: 'center' }}><div style={{marginTop: 200}}><Loader type='Grid' color='#023E8A' /></div></div>)
   // if(error) return (<div style={{ marginTop: 300, textAlign: 'center' }}><h3>Empty</h3></div>)
   return (
-    <div style={{marginTop: 50}}>
-      <p className="text-center main-porto-title mb-0">Commissions</p>
+    <div style={{background: cardbg }}>
+        <div style={{height: 50}}></div>
+        <p className="text-center main-porto-title mb-0">Commissions</p>
       <div className='cards-content-holder'>
           {commissions.map(card => {
             return  <CommissionCard card={card} key={card.id} />
--- client/src/components/MainWork.js
+++ client/src/components/MainWork.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { getAllWorks } from '../store/actions'
 import { useSelector, useDispatch } from 'react-redux';
 import WorkCard from './WorkCard'
@@ -6,19 +6,25 @@ import Loader from 'react-loader-spinner';
 import './MainWorkCommission.css';
 
 const MainWork = () => {
-
+  const [cardbg, setCardbg] = useState('#DBF5FA')
   const works = useSelector((state) => state.works)
   const loading = useSelector((state) => state.loading)
   const dispatch = useDispatch()
 
   useEffect(() => {
+    if (localStorage.darkmode =='dark') {
+        setCardbg('black')
+      } else {
+        setCardbg('#DBF5FA')
+      }
       dispatch(getAllWorks())
   }, []);
 
   if(loading) return (<div style={{ marginTop: 200, textAlign: 'center' }}> <Loader type='Grid' color='#023E8A' /> </div>)
   // if(error) return (<div style={{ marginTop: 100, textAlign: 'center' }}><h3>Empty</h3></div>)
   return (
-    <div style={{ marginTop: 50 }}>
+    <div style={{ background: cardbg }}>
+      <div style={{height: 50}}></div>
       <p className="text-center main-porto-title mb-0">Portfolios</p>
       <div className='cards-content-holder'>
           {works.map(card => {
