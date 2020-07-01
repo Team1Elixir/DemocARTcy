0f449410f0b48cb5a2f3f718064524cdc74cb046
achrams <ramadhanovic@gmail.com>
2020-07-01 17:46:24 +0700

    other dev

--- client/src/components/UserCommission.js
+++ client/src/components/UserCommission.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { getUserCommissions,getUserData } from '../store/actions'
 import { Link } from 'react-router-dom'
@@ -20,7 +20,7 @@ const UserCommission = () => {
   if(loading) return (<div style={{ marginTop: 200, textAlign: 'center' }}> <Loader type='Grid' color='#023E8A' /> </div>)
 
   if (error) return (
-    <div style={{ marginTop: 50 }}>
+    <div>
         <div className='buttonpanel'>
           <h2 style={{ textAlign: 'center' }}>My Commissions</h2>
             <Link className='btn btn-primary add-new' to ='/commissions/add'>+ Add</Link>
@@ -30,12 +30,13 @@ const UserCommission = () => {
   )
 
    return (
-   <div style={{ marginTop: 50 }}>
+   <div>
+		<div style={{height: 100}}></div>
         <div className='buttonpanel d-flex flex-column'>
           <h2 style={{ textAlign: 'center' }} className="user-area-title mb-0">My Commissions</h2>
             <Link className='btn btn-bluish add-new' to ='/commissions/add'><i class="fas fa-plus"></i> Add Commission</Link>
         </div>
-        <div className='cards-content-holder mb-5'>
+        <div className='cards-content-holder mb-5' style={{ height: 'inherit' }}>
           {commissions.map(card => {
             return  <CommissionCard card={card} key={card.id} />
           })
